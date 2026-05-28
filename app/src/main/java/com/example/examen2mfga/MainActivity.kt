6e772ef8e68109844ae3869857b519520c3cba72
fershggs <321287041@comunidad.unam.mx>
2026-05-27 21:32:39 -0600

    Validación de contraseña

--- app/src/main/java/com/example/examen2mfga/MainActivity.kt
+++ app/src/main/java/com/example/examen2mfga/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.examen2mfga
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import android.content.Intent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -23,6 +25,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnSesion.setOnClickListener {
+            val password = binding.edtContraseA.text.toString()
+
+            if (password == "abc123") {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            }else {
+                // La contraseña es incorrecta, mostrar Toast
+                Toast.makeText(this, "No es correcta la contraseña", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
---
+++ app/src/main/java/com/example/examen2mfga/ProfileActivity.kt
@@ -0,0 +1,28 @@
+package com.example.examen2mfga
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.examen2mfga.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
